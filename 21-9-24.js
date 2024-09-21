4385421fb6838cb51721c6b0d83e8f0c46cae9ad
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-09-21 21:16:22 +0500

    Add files via upload

---
+++ 21-9-24.js
@@ -0,0 +1,47 @@
+
+const awais = {
+    age : 23,
+    hobby : "reading",
+
+}
+// console.log(awais);
+
+// const myArray = ["this" , "is" , "an"]
+// console.log(myArray[7]);
+
+
+// ARRAY OF OBJECTS
+
+// let students = [{ name : "aws" , class : 12} , { name : 'dilawar' , class: 13}]
+// console.log(students[1].class);
+
+
+// OBJECT OF ARRAYS
+
+// let semesters = { bscs : [{ name : "aws" , class : 12} , { name : 'dilawar' , class: { bscs : [{ name : "aws" , class : 12} , { name : 'dilawar' , class: 13}] , bba :  [ "c1" , "c2" , "c3" , "c7" , "c890" ]}}] , bba :  [ "c1" , "c2" , "c3" , "c7" , "c890" ]}
+// console.log(semesters.bscs[1].class.bscs[1].class);
+
+// function METHOD 1
+
+// function sqr (number){
+
+//     console.log(number * number -2 ); 
+// }
+
+// sqr(5);
+// sqr(5 == 5);
+
+//METHOD 2
+
+const sum =  (num1 , num2) =>{
+
+    console.log(num1 + num2);
+    
+}
+
+
+let x = 2 , y = 3
+sum(9 , y)
+
+
+
